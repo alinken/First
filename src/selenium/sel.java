4ec8212a6dcd739935e5284170b22443afd62278
alinken75@gmail.com <alinaken@Alinas-MacBook-Air.local>
2020-05-22 11:34:29 -0500

    selenium project

---
+++ src/selenium/sel.java
@@ -0,0 +1,24 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class sel {
+
+
+		
+		 public static void main(String[] args) {
+				System.setProperty("webdriver.chrome.driver", "/Users/alinaken/Documents/chromedriver");
+
+				WebDriver driver = new ChromeDriver();
+			
+				driver.get("https://www.facebook.com/");
+				
+				
+				
+			 
+			 
+	
+}
+}
